86780cb9831528463144d72fd2d57de0bf1b213a
Snugglez <32309975+Snugglez@users.noreply.github.com>
2018-02-12 23:00:47 -0500

    Switched to official def name

--- README.md
+++ README.md
@@ -15,15 +15,6 @@ The commands should be pretty self explanatory
 
 Exclude the "!" if used in /8 or proxy chat channel
 
-Extra installation (until its properly added in tera-data)
+Properly added opcode/def to https://github.com/meishuu/tera-data so if the module no longer functions, update tera-data
 
-included is the def C_GUARDIAN_TP (name is wip, I think the final name will be something like C_REQUEST_FIELD_EVENT_TELEPORT)
-
-Place the def inside `tera-proxy/node_modules/tera-data/protocol` NOT THE node_modules FOLDER INSIDE BIN
-
-Place the below opcode for your region in your opcode maps in `tera-proxy/node_modules/tera-data/map` 
-
-NA = protocol.326439.map
-
-NA OPCODE
-`C_GUARDIAN_TP = 54316`
+Only mapped the opcode in na, so if you mapped it in another region look again but now as `C_REQUEST_FIELD_EVENT_TELEPORT` its official packet name
--- index.js
+++ index.js
@@ -3,37 +3,37 @@ module.exports = function guardiantp(dispatch) {
 const command = Command(dispatch)
 
 command.add('gut', () => {
-dispatch.toServer('C_GUARDIAN_TP', 1, {
+dispatch.toServer('C_REQUEST_FIELD_EVENT_TELEPORT', 1, {
 zone: 7011,
 type: 1
 })})
 
 command.add('bleak', () => {
-dispatch.toServer('C_GUARDIAN_TP', 1, {
+dispatch.toServer('C_REQUEST_FIELD_EVENT_TELEPORT', 1, {
 zone: 7012,
 type: 1
 })})
 
 command.add('eyes', () => {
-dispatch.toServer('C_GUARDIAN_TP', 1, {
+dispatch.toServer('C_REQUEST_FIELD_EVENT_TELEPORT', 1, {
 zone: 7003,
 type: 1
 })})
 
 command.add('tu', () => {
-dispatch.toServer('C_GUARDIAN_TP', 1, {
+dispatch.toServer('C_REQUEST_FIELD_EVENT_TELEPORT', 1, {
 zone: 7001,
 type: 2
 })})
 
 command.add('crab', () => {
-dispatch.toServer('C_GUARDIAN_TP', 1, {
+dispatch.toServer('C_REQUEST_FIELD_EVENT_TELEPORT', 1, {
 zone: 7014,
 type: 2
 })})
 
 command.add('balder', () => {
-dispatch.toServer('C_GUARDIAN_TP', 1, {
+dispatch.toServer('C_REQUEST_FIELD_EVENT_TELEPORT', 1, {
 zone: 7015,
 type: 1
 })})
